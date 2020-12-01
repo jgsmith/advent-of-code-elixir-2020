59fb17001a7fca7f8739a913a0d99e0fbdf5677f
James Smith <jgsmith@gmail.com>
2020-12-01 14:48:29 -0500

    Day 1

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,84 @@
 defmodule AdventOfCode.Day01 do
-  def part1(args) do
+  @type expenses :: [integer]
+  @type expense_set :: [integer]
+  @type expense_sets :: [expense_set]
+  @type context :: {expenses, integer}
+
+  def part1(args) when is_list(args) do
+    IO.inspect(find_product(2, new_context(args, 2020)))
   end
 
-  def part2(args) do
+  def part1(filename) when is_binary(filename) do
+    filename
+    |> read_from_file()
+    |> part1()
+  end
+
+  def part1(), do: part1("./data/day01.txt")
+
+  def part2(args) when is_list(args) do
+    IO.inspect(find_product(3, new_context(args, 2020)))
+  end
+
+  def part2(filename) when is_binary(filename) do
+    filename
+    |> read_from_file()
+    |> part2()
+  end
+
+  def part2(), do: part2("./data/day01.txt")
+
+  def read_from_file(filename) do
+    filename
+    |> File.read!()
+    |> String.split(~r{\s*\n\s*})
+    |> Enum.reject(fn s -> Regex.match?(~r/^\s*$/, s) end)
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  @spec new_context(expenses, integer) :: context
+  def new_context(expenses, target) do
+    {Enum.sort(expenses), target}
+  end
+
+  @spec find_product(integer, context) :: integer
+  def find_product(count, context) do
+    list = find_sum(count, context)
+    Enum.reduce(list, 1, fn x, y -> x * y end)
+  end
+
+  @spec find_sum(integer, context) :: [integer]
+  def find_sum(count, context) do
+    [[]]
+    |> build_possible_terms(count, context)
+    |> Enum.find(&sum_matching?(&1, context))
+  end
+
+  @spec sum_matching?(expense_set, context) :: bool
+  def sum_matching?(list, {_, target}) do
+    if Enum.any?(list, &is_nil/1) do
+      false
+    else
+      Enum.sum(list) == target
+    end
+  end
+
+  @spec build_possible_terms(expense_sets, integer, context) :: [[integer]]
+  def build_possible_terms(terms, 0, _), do: terms
+
+  def build_possible_terms(terms, count, context) do
+    terms
+    |> Enum.flat_map(&collect_next_sets(&1, context))
+    |> build_possible_terms(count - 1, context)
+  end
+
+  @spec collect_next_sets(expense_set, context) :: [[integer]]
+  def collect_next_sets(term, {sorted_expenses, target}) do
+    used = Enum.sum(term)
+    available = target - used
+
+    sorted_expenses
+    |> Enum.take_while(&(&1 <= available))
+    |> Enum.map(&[&1 | term])
   end
 end
--- test/advent_of_code/day_01_test.exs
+++ test/advent_of_code/day_01_test.exs
@@ -3,19 +3,37 @@ defmodule AdventOfCode.Day01Test do
 
   import AdventOfCode.Day01
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  test "new_context" do
+    input = [1721, 979, 366, 299, 675, 1456]
 
-    assert result
+    assert new_context(input, 2020) == {Enum.sort(input), 2020}
   end
 
-  @tag :skip
-  test "part2" do
-    input = nil
-    result = part2(input)
+  test "build_possible_terms" do
+    input = [1721, 979, 366, 299, 675, 1456]
+    context = new_context(input, 2020)
 
-    assert result
+    result = build_possible_terms([[299]], 1, context)
+    assert result == [[299, 299], [366, 299], [675, 299], [979, 299], [1456, 299], [1721, 299]]
+  end
+
+  test "find_sum" do
+    context = new_context([1721, 979, 366, 299, 675, 1456], 2020)
+
+    result = find_sum(2, context)
+    assert result == [1721, 299]
+
+    result = find_sum(3, context)
+    assert result == [979, 675, 366]
+  end
+
+  test "find_product" do
+    context = new_context([1721, 979, 366, 299, 675, 1456], 2020)
+
+    result = find_product(2, context)
+    assert result == 514_579
+
+    result = find_product(3, context)
+    assert result == 241_861_950
   end
 end
