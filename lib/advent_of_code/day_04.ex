48482172e2f8b37e0d8ba87c3287e676237bf278
James Smith <jgsmith@gmail.com>
2020-12-04 13:07:05 -0500

    Day 4

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,7 +1,99 @@
 defmodule AdventOfCode.Day04 do
-  def part1(args) do
+  @required_keys MapSet.new(~w[byr iyr eyr hgt hcl ecl pid]a)
+
+  def part1(args) when is_list(args), do: Enum.count(args, &valid_passport?/1)
+
+  def part1(filename) when is_binary(filename) do
+    filename
+    |> read_from_file()
+    |> part1()
   end
 
-  def part2(args) do
+  def part1(), do: part1("./data/day04.txt")
+
+  def part2(args) when is_list(args), do: Enum.count(args, &really_valid_passport?/1)
+
+  def part2(filename) when is_binary(filename) do
+    filename
+    |> read_from_file()
+    |> part2()
   end
+
+  def part2(), do: part2("./data/day04.txt")
+
+  def read_from_file(filename) do
+    filename
+    |> File.read!()
+    |> parse_file()
+  end
+
+  def parse_file(content) when is_binary(content) do
+    content
+    |> String.split(~r{\s*\n\s*\n\s*})
+    |> Enum.reject(&String.match?(&1, ~r/^\s*$/))
+    |> Enum.map(&parse_passport/1)
+  end
+
+  def parse_passport(chunk) do
+    chunk
+    |> String.split(~r{\s+})
+    |> Enum.reject(fn x -> x == "" end)
+    |> Map.new(fn <<key::binary-3, ":", value::binary>> ->
+      {String.to_atom(key), value}
+    end)
+  end
+
+  def valid_passport?(passport) do
+    keys = MapSet.new(Map.keys(passport))
+    MapSet.size(MapSet.difference(@required_keys, keys)) == 0
+  end
+
+  def really_valid_passport?(passport) do
+    valid_birthdate?(passport) and
+      valid_issue_year?(passport) and
+      valid_expiration_year?(passport) and
+      valid_height?(passport) and
+      valid_hair_color?(passport) and
+      valid_eye_color?(passport) and
+      valid_passport_id?(passport)
+  end
+
+  def valid_four_digit_number?(n, min, max) do
+    if String.length(n) == 4 do
+      case Integer.parse(n) do
+        {int, ""} -> max >= int and int >= min
+        _ -> false
+      end
+    else
+      false
+    end
+  end
+
+  def valid_birthdate?(%{byr: byr}), do: valid_four_digit_number?(byr, 1920, 2002)
+  def valid_birthdate?(_), do: false
+
+  def valid_issue_year?(%{iyr: iyr}), do: valid_four_digit_number?(iyr, 2010, 2020)
+  def valid_issue_year?(_), do: false
+
+  def valid_expiration_year?(%{eyr: eyr}), do: valid_four_digit_number?(eyr, 2020, 2030)
+  def valid_expiration_year?(_), do: false
+
+  def valid_height?(%{hgt: hgt}) do
+    case Integer.parse(hgt) do
+      {cm, "cm"} -> 193 >= cm and cm >= 150
+      {inches, "in"} -> 76 >= inches and inches >= 59
+      _ -> false
+    end
+  end
+
+  def valid_height?(_), do: false
+
+  def valid_hair_color?(%{hcl: hcl}), do: Regex.match?(~r/^\#[0-9a-f]{6}$/, hcl)
+  def valid_hair_color?(_), do: false
+
+  def valid_eye_color?(%{ecl: ecl}), do: ecl in ~w[amb blu brn gry grn hzl oth]
+  def valid_eye_color?(_), do: false
+
+  def valid_passport_id?(%{pid: pid}), do: Regex.match?(~r/^[0-9]{9}$/, pid)
+  def valid_passport_id?(_), do: false
 end
--- test/advent_of_code/day_04_test.exs
+++ test/advent_of_code/day_04_test.exs
@@ -3,19 +3,90 @@ defmodule AdventOfCode.Day04Test do
 
   import AdventOfCode.Day04
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input =
+      parse_file("""
+      ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+      byr:1937 iyr:2017 cid:147 hgt:183cm
+
+      iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+      hcl:#cfa07d byr:1929
+
+      hcl:#ae17e1 iyr:2013
+      eyr:2024
+      ecl:brn pid:760753108 byr:1931
+      hgt:179cm
+
+      hcl:#cfa07d eyr:2025 pid:166559648
+      iyr:2011 ecl:brn hgt:59in
+      """)
+
     result = part1(input)
 
-    assert result
+    assert result == 2
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input =
+      parse_file("""
+      eyr:1972 cid:100
+      hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+      iyr:2019
+      hcl:#602927 eyr:1967 hgt:170cm
+      ecl:grn pid:012533040 byr:1946
+
+      hcl:dab227 iyr:2012
+      ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+      hgt:59cm ecl:zzz
+      eyr:2038 hcl:74454a iyr:2023
+      pid:3556412378 byr:2007
+
+      pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+      hcl:#623a2f
+
+      eyr:2029 ecl:blu cid:129 byr:1989
+      iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+      hcl:#888785
+      hgt:164cm byr:2001 iyr:2015 cid:88
+      pid:545766238 ecl:hzl
+      eyr:2022
+
+      iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+      """)
+
     result = part2(input)
 
-    assert result
+    assert result == 4
+  end
+
+  test "valid_birthdate?" do
+    assert valid_birthdate?(%{byr: "2002"})
+    assert not valid_birthdate?(%{byr: "2003"})
+  end
+
+  test "valid_height?" do
+    assert valid_height?(%{hgt: "60in"})
+    assert valid_height?(%{hgt: "190cm"})
+    assert not valid_height?(%{hgt: "190in"})
+    assert not valid_height?(%{hgt: "190"})
+  end
+
+  test "valid_hair_color?" do
+    assert valid_hair_color?(%{hcl: "#123abc"})
+    assert not valid_hair_color?(%{hcl: "#123abz"})
+    assert not valid_hair_color?(%{hcl: "123abc"})
+  end
+
+  test "valid_eye_color?" do
+    assert valid_eye_color?(%{ecl: "brn"})
+    assert not valid_eye_color?(%{ecl: "wat"})
+  end
+
+  test "valid_passport_id?" do
+    assert valid_passport_id?(%{pid: "000000001"})
+    assert not valid_passport_id?(%{pid: "0123456789"})
   end
 end
