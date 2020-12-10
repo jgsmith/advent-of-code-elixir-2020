805c5f0b4bb19e9a0a85e54e77e7b557a6fa5211
James Smith <jgsmith@gmail.com>
2020-12-10 15:51:42 -0500

    Day 10

--- lib/advent_of_code/day_10.ex
+++ lib/advent_of_code/day_10.ex
@@ -1,7 +1,101 @@
 defmodule AdventOfCode.Day10 do
-  def part1(args) do
+  def part1(), do: part1("./data/day10.txt")
+
+  def part1(filename) when is_binary(filename) do
+    filename
+    |> read_from_file()
+    |> part1()
+  end
+
+  def part1(args) when is_list(args) do
+    count_differences(args, 1) * (1 + count_differences(args, 3))
+  end
+
+  def part2(), do: part2("./data/day10.txt")
+
+  def part2(filename) when is_binary(filename) do
+    filename
+    |> read_from_file()
+    |> part2()
   end
 
   def part2(args) do
+    count_ways([0 | args])
+  end
+
+  def read_from_file(filename) do
+    filename
+    |> File.read!()
+    |> parse_file()
+  end
+
+  def parse_file(content) do
+    content
+    |> String.split(~r{\s*\n\s*}, trim: true)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.sort()
+  end
+
+  def count_differences(joltages, diff) do
+    [0 | joltages]
+    |> Enum.zip(joltages)
+    |> Enum.count(fn {x, y} -> y - x == diff end)
+  end
+
+  def count_ways(joltages) do
+    graph = build_graph(joltages)
+    annotate_nodes_with_counts(graph)
+    {_, count} = :digraph.vertex(graph, 0)
+    count
+  end
+
+  def build_graph(joltages) do
+    graph = :digraph.new()
+    build_graph(graph, joltages)
+  end
+
+  def build_graph(graph, []), do: graph
+
+  def build_graph(graph, [root | rest]) do
+    reachable = Enum.take_while(rest, fn x -> x - root <= 3 end)
+    root_v = :digraph.add_vertex(graph, root, 0)
+
+    for node <- reachable do
+      node_v = :digraph.add_vertex(graph, node, 0)
+      :digraph.add_edge(graph, root_v, node_v)
+    end
+
+    build_graph(graph, rest)
+  end
+
+  def annotate_nodes_with_counts(graph) do
+    leaves = get_leaves(graph)
+
+    for leaf <- leaves do
+      :digraph.add_vertex(graph, leaf, 1)
+    end
+
+    nodes =
+      graph
+      |> :digraph_utils.topsort()
+      |> Enum.reverse()
+
+    for node <- nodes do
+      {_, count} = :digraph.vertex(graph, node)
+      parent_nodes = :digraph.in_neighbours(graph, node)
+
+      for parent_node <- parent_nodes do
+        {_, n} = :digraph.vertex(graph, parent_node)
+        :digraph.add_vertex(graph, parent_node, n + count)
+      end
+    end
+  end
+
+  def get_leaves(graph) do
+    graph
+    |> :digraph.vertices()
+    |> Enum.filter(fn v ->
+      :digraph.out_degree(graph, v) == 0
+    end)
   end
 end
--- test/advent_of_code/day_10_test.exs
+++ test/advent_of_code/day_10_test.exs
@@ -3,19 +3,89 @@ defmodule AdventOfCode.Day10Test do
 
   import AdventOfCode.Day10
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input =
+      parse_file("""
+      28
+      33
+      18
+      42
+      31
+      14
+      46
+      20
+      48
+      47
+      24
+      23
+      49
+      45
+      19
+      38
+      39
+      11
+      1
+      32
+      25
+      35
+      8
+      17
+      7
+      9
+      4
+      2
+      34
+      10
+      3
+      """)
+
     result = part1(input)
 
-    assert result
+    assert result == 22 * 10
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = [1, 4, 5, 6, 7, 10, 11, 12, 15, 16, 19]
+    result = part2(input)
+    assert result == 8
+
+    input =
+      parse_file("""
+      28
+      33
+      18
+      42
+      31
+      14
+      46
+      20
+      48
+      47
+      24
+      23
+      49
+      45
+      19
+      38
+      39
+      11
+      1
+      32
+      25
+      35
+      8
+      17
+      7
+      9
+      4
+      2
+      34
+      10
+      3
+      """)
+
     result = part2(input)
 
-    assert result
+    assert result == 19208
   end
 end
